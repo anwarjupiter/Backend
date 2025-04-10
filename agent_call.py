9e56ea9d3e081074898109edcd616cc3dd3d38c0
anwarjupiter <anwar@jupiterbrothers.com>
2025-04-10 15:04:49 +0530

    Add PDF processing and random number generation tools
    - Implemented PDFInput model for structured input handling.
    - Created process_pdf_tool_structured function to summarize PDF content based on a question.
    - Added random_number function to generate random integers.
    - Defined StructuredTool instances for PDF processing and random number generation.
    - Initialized WatsonxLLM with project and API credentials.
    - Set up a structured chat agent using the defined tools and LLM.

---
+++ agent_call.py
@@ -0,0 +1,61 @@
+from langchain.agents import initialize_agent
+from langchain.agents.agent_types import AgentType
+from langchain_ibm import WatsonxLLM
+from constants import *
+from langchain.tools import StructuredTool
+from pydantic import BaseModel
+import random
+from agents import pdf
+
+class PDFInput(BaseModel):
+    question: str
+    pdf_path: str
+    file_path:str
+
+
+def process_pdf_tool_structured(input: PDFInput) -> str:
+    return pdf.run(question=input.question,pdf_path=input.pdf_path,vector_db=input.file_path)
+
+def random_number(input:str) -> str:
+    return random.randint(1,5)
+
+pdf_tool = StructuredTool.from_function(
+    func=process_pdf_tool_structured,
+    name="PDFTool",
+    description="Summarize PDF files based on a question and the file content."
+)
+
+random_tool = StructuredTool.from_function(
+    func=random_number,
+    name="RandomNumberTool",
+    description="useful to generate a random number quickly"
+)
+
+tools = [
+    pdf_tool,
+    random_tool
+]
+
+llm = WatsonxLLM(
+        model_id=MODEL_GRANITE_8B,
+        project_id=WATSONX_PROJECT_ID,
+        apikey=WATSONX_API_KEY,
+        url=SERVER_URL,
+        params=WASTSONX_PARAMS
+    )
+
+react_agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True
+)
+
+# tool_input = {
+#     "question": "what is assembly building ?",
+#     "pdf_path": "input/tn.pdf"
+# }
+
+
+# result = react_agent.run(input=tool_input)
+# print(result)
