372df6b35bbfd90a508e23489c01c009dc9d05a4
anwarjupiter <anwar@jupiterbrothers.com>
2025-04-17 14:48:03 +0530

    fix: update IBM_MODEL assignment for consistency in constants.py

--- constants.py
+++ constants.py
@@ -13,8 +13,10 @@ MODEL_GRANITE_13B = "ibm/granite-13b-instruct-v2"
 MODEL_GRANITE_8B = "ibm/granite-3-8b-instruct"
 MODEL_LAMA_70B = "meta-llama/llama-3-1-70b-instruct"
 MODEL_GRANITE_8B_CODE = "ibm/granite-8b-code-instruct"
+MISTRAL_LARGE = "mistralai/mistral-large"
 
-IBM_MODEL = "mistralai/mistral-large"
+IBM_MODEL = MISTRAL_LARGE
+
 WASTSONX_PARAMS = {
     "temperature": 0,
     "max_new_tokens": 500,
